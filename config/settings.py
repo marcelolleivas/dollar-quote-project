6bebdc0d92ff6dc27a9b1eff08c30dfc1fee4536
marcelolleivas <marcelo.lleivas@gmail.com>
2022-10-25 17:44:43 -0300

    setup.cfg update

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -30,6 +30,8 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
+
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "dollar_quote.rates",
+    "celerybeat_status",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(SETTINGS_PATH, "dollar_quote", "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -115,9 +119,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+MEDIA_URL = "dollar_quote/media/"
+MEDIA_ROOT = "dollar_quote/media"
+
+STATIC_URL = "dollar_quote/static/"
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, "dollar_quote", "static")]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, "dollar_quote", "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Celery settings
+CELERY_BROKER_URL = "redis://localhost:6379"
+CELERY_RESULT_BACKEND = "redis://localhost:6379"
--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path, re_path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("dollar_quote.urls")),
+    re_path(
+        r"^admin/statuscheck/",
+        include("celerybeat_status.urls", namespace="celerybeat_status"),
+    ),
 ]
