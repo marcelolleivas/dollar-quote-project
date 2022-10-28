7ad5728651c6bc6c009fd47cd201d5292cf74271
marcelolleivas <marcelo.lleivas@gmail.com>
2022-10-28 09:44:12 -0300

    improves query to ignore when Integrety Error

--- dollar_quote/rates/management/commands/populate_database.py
+++ dollar_quote/rates/management/commands/populate_database.py
@@ -1,7 +1,6 @@
 from datetime import datetime, timedelta
 
 from django.core.management import BaseCommand
-from django.db import IntegrityError
 
 from dollar_quote.rates.models import Rate
 from dollar_quote.services import VATService
@@ -24,7 +23,5 @@ class Command(BaseCommand):  # pragma: no cover
                     japanese_yen=service_data["rates"]["JPY"],
                 )
             )
-        try:
-            Rate.objects.bulk_create(data_to_db)
-        except IntegrityError:
-            return
+
+        Rate.objects.bulk_create(data_to_db, ignore_conflicts=True)
